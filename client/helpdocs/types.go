26a8abec5af95327815ad835ef771416ba6e2117
Timucin Gelici <timucin.gelici@gmail.com>
2019-03-27 16:07:00 +0000

    Fix the indentation

--- client/helpdocs/types.go
+++ client/helpdocs/types.go
@@ -2,37 +2,35 @@ package helpdocs
 
 // Helpdocs Components
 type Search struct {
-	Articles []Article	`json:"articles"`
-	Query	 string		`json:"query"`
-	QueryTruncated bool	`json:"query_truncated"`
-	TotalHits int		`json:"totalHits"`
-	MaxScore float64	`json:"maxScore"`
-	StemmedQuery string	`json:"stemmedQuery"`
+	Articles       []Article `json:"articles"`
+	Query          string    `json:"query"`
+	QueryTruncated bool      `json:"query_truncated"`
+	TotalHits      int       `json:"totalHits"`
+	MaxScore       float64   `json:"maxScore"`
+	StemmedQuery   string    `json:"stemmedQuery"`
 }
 
-// Just a not on the article structure in the search is different then the article we get from the single article
-// endpoint but this structure is designed to be enough to hold the necessary information from both structures
 type Article struct {
-	AccountId 		string	`json:"account_id"`
-	ArticleId 		string	`json:"article_id"`
-	Body			string	`json:"body"`
-	Canonical 		string	`json:"canonical"`
-	CategoryTitle 	string	`json:"category_title"`
-	Description 	string	`json:"description"`
-	IsPrivate 		bool	`json:"is_private"`
-	IsPublished 	bool	`json:"is_published"`
-	PermissionGroups []string `json:"permission_groups"`
-	RelativeUrl 	string	`json:"relative_url"`
-	Search 			SearchScore	`json:"search"`
-	ShortVersion 	string	`json:"short_version"`
-	Slug 			string	`json:"slug"`
-	Tags 			[]string `json:"tags"`
-	Title 			string	`json:"title"`
-	Url 			string	`json:"url"`
+	AccountId        string      `json:"account_id"`
+	ArticleId        string      `json:"article_id"`
+	Body             string      `json:"body"`
+	Canonical        string      `json:"canonical"`
+	CategoryTitle    string      `json:"category_title"`
+	Description      string      `json:"description"`
+	IsPrivate        bool        `json:"is_private"`
+	IsPublished      bool        `json:"is_published"`
+	PermissionGroups []string    `json:"permission_groups"`
+	RelativeUrl      string      `json:"relative_url"`
+	Search           SearchScore `json:"search"`
+	ShortVersion     string      `json:"short_version"`
+	Slug             string      `json:"slug"`
+	Tags             []string    `json:"tags"`
+	Title            string      `json:"title"`
+	Url              string      `json:"url"`
 }
 
 type ArticleResponse struct {
-	Article	Article `json:"article"`
+	Article Article `json:"article"`
 }
 
 type SearchScore struct {
--- client/intercom/types.go
+++ client/intercom/types.go
@@ -5,7 +5,7 @@ type Canvas struct {
 }
 
 type CanvasStatic struct {
-	Content Content `json:"content"`
+	Content    Content     `json:"content"`
 	StoredData interface{} `json:"stored_data"`
 }
 
@@ -14,42 +14,41 @@ type Content struct {
 }
 
 type InputComponent struct {
-	Type 		string	`json:"type"`
-	Id			string	`json:"id"`
-	Label		string	`json:"label"`
-	Placeholder	string	`json:"placeholder"`
-	Value		string	`json:"value"`
-	SaveState	string	`json:"save_state"`
-	Disabled	bool	`json:"disabled"`
-	Action 		SubmitAction `json:"action"`
+	Type        string       `json:"type"`
+	Id          string       `json:"id"`
+	Label       string       `json:"label"`
+	Placeholder string       `json:"placeholder"`
+	Value       string       `json:"value"`
+	SaveState   string       `json:"save_state"`
+	Disabled    bool         `json:"disabled"`
+	Action      SubmitAction `json:"action"`
 }
 
 type SubmitAction struct {
-	Type 	string	`json:"type"`
+	Type string `json:"type"`
 }
 
 type SheetAction struct {
-	Type	string `json:"type"`
-	Url		string `json:"url"`
+	Type string `json:"type"`
+	Url  string `json:"url"`
 }
 
 type Request struct {
-	AppId	string `json:"app_id"`
-	CurrentCanvas	struct{
-		Content	Content
+	AppId         string `json:"app_id"`
+	CurrentCanvas struct {
+		Content Content
 	} `json:"current_canvas"`
-	ComponentId	string `json:"component_id"`
+	ComponentId string            `json:"component_id"`
 	InputValues map[string]string `json:"input_values"`
-	User struct {
+	User        struct {
 		CustomAttributes struct {
-			Region	string `json:"region"`
-			Role	string `json:"role"`
-			Roles	string `json:"roles"`
-			Source	string `json:"source"`
-			Tenant	string `json:"tenant"`
+			Region string `json:"region"`
+			Role   string `json:"role"`
+			Roles  string `json:"roles"`
+			Source string `json:"source"`
+			Tenant string `json:"tenant"`
 		} `json:"custom_attributes"`
 	} `json:"user"`
-
 }
 
 type SearchRequest struct {
@@ -60,22 +59,22 @@ type SearchRequest struct {
 }
 
 type ListResponse struct {
-	Type 		string			`json:"type"`
-	Disabled 	bool			`json:"disabled"`
-	Items 		[]ListResponseItem	`json:"items"`
+	Type     string             `json:"type"`
+	Disabled bool               `json:"disabled"`
+	Items    []ListResponseItem `json:"items"`
 }
 
 type ListResponseItem struct {
-	Type 		string `json:"type"`
-	Id			string `json:"id"`
-	Title		string `json:"title"`
-	Subtitle	string `json:"subtitle"`
-	Image		string `json:"image"`
-	ImageWidth	int `json:"image_width"`
-	ImageHeight	int `json:"image_height"`
-	RoundedImage	bool `json:"rounded_image"`
-	Disabled	bool `json:"disabled"`
-	Action		interface{} `json:"action"`
+	Type         string      `json:"type"`
+	Id           string      `json:"id"`
+	Title        string      `json:"title"`
+	Subtitle     string      `json:"subtitle"`
+	Image        string      `json:"image"`
+	ImageWidth   int         `json:"image_width"`
+	ImageHeight  int         `json:"image_height"`
+	RoundedImage bool        `json:"rounded_image"`
+	Disabled     bool        `json:"disabled"`
+	Action       interface{} `json:"action"`
 }
 
 type ConfigResults struct {
